d8c25515c6c913c41248aecf0a9ac0b02d30af2c
rosa.juliana.vargas.tucto@gmail.com <rosa.juliana.vargas.tucto@gmail.com>
2021-10-25 20:29:00 -0500

    g

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template>  
+  <PokemonPage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PokemonPage from './pages/PokemonPage'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PokemonPage
   }
 }
 </script>
@@ -24,3 +23,4 @@ export default {
   margin-top: 60px;
 }
 </style>
+
